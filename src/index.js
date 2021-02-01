0896fdc56855b65950ff90cb39e92045f05eb38e
Andrew Pritula <andreylritula@gmail.com>
2021-02-01 17:00:45 +0200

    task1, create react project + add html code to component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import {render} from 'react-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './style.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import VCS from '../src/pages/VCS'
+import Git from '../src/pages/Git'
+import NodeJS from '../src/pages/NodeJS'
+import Npm from '../src/pages/Npm'
+import Html from '../src/pages/Html'
+import Css from '../src/pages/Css'
+
+function Page() {
+    return (
+        <div id='page'>
+            <header>
+                <h1>Testproject</h1>
+            </header>
+            <section class="overview">
+	            <h2>Testproject for onix internship</h2>
+            </section>
+            <nav>
+                <ul>
+                    <li><a href="/">VCS</a></li>
+                    <li><a href="/git">Git</a></li>
+	                <li><a href="/node">NodeJS</a></li>
+                    <li><a href="/npm">Npm</a></li>
+	                <li><a href="/html">Html</a></li>
+                    <li><a href="/css">CSS</a></li>
+                </ul>
+            </nav>
+  
+            <article id='article'>
+                <Router>
+                    <Switch>
+                        <Route exact path = "/" component={VCS}/>
+                        <Route exact path = "/git" component={Git}/>
+                        <Route exact path = "/node" component={NodeJS}/>
+                        <Route exact path = "/npm" component={Npm}/>
+                        <Route exact path = "/html" component={Html}/>
+                        <Route exact path = "/css" component={Css}/>
+                    </Switch>
+                </Router>
+            </article>
+
+            <footer>
+                <ul>
+                    <li>Andrew Pritula</li>
+                    <li><a href="https://github.com/andrewpritula">GitHub profile</a></li>
+                    <li><a href="https://dribbble.com/shots/14867346-Harman">Original design</a></li>
+                </ul>
+            </footer>
+        </div>
+    )
+}
+
+render(<Page/>, document.getElementById('root'));
