62e327de30b08ebb17e53def7f7fd64f5747bbb8
Andrew Pritula <andreylritula@gmail.com>
2021-03-28 01:57:32 +0200

    task 7 add onClick that changes bgColor of table item on  active/default

--- src/Task6.js
+++ src/Task6.js
@@ -22,6 +22,12 @@ class Task6 extends Component {
     }
   }
   
+  changeColor = () => {
+    this.state.selected === false 
+      ? this.setState({ selected: true , color : 'green'}) 
+      : this.setState({ selected: false , color : '#ff837b'});
+  };
+
   render() {
     const articles = this.state.data;
     return (
@@ -37,18 +43,47 @@ class Task6 extends Component {
               </tr>
           </thead>
           <tbody key ='tbody'>
-            {articles.map((item) => 
-              <tr key={item.title}>
-                <td key>{item.title}</td>
-                <td>{item.publishedAt}</td>
-                <td>{item.updatedAt}</td>
-                <td>{item.summary}</td>
-                <td><a className='table-link' href={item.url}>Link</a></td>
-              </tr>
-            )}
+            {articles.map((item, index) => 
+            <Item
+            key={index}
+            item={item}
+            />
+            )};
           </tbody>
         </table>
       </div>
+    );
+  }
+}
+
+
+class Item extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: "#ff837b",
+      selected: false
+    };
+  }
+
+  changeColor = () => {
+    this.state.selected === false 
+      ? this.setState({ selected: true , color : 'green'}) 
+      : this.setState({ selected: false , color : '#ff837b'});
+  };
+
+  render() {
+    return (
+      <tr 
+      style={{ backgroundColor: this.state.color, cursor: 'pointer' }}
+      onClick={() => this.changeColor()}>
+        <td>{this.props.item.title}</td>
+        <td>{this.props.item.publishedAt}</td>
+        <td>{this.props.item.updatedAt}</td>
+        <td>{this.props.item.summary}</td>
+        <td><a className='table-link' href={this.props.item.url}>Link</a></td>
+      </tr>
     );
   }
 }
--- src/Task7.js
+++ src/Task7.js
@@ -13,97 +13,9 @@ class List extends Component {
     super(props);
     this.state = 
     {
-      ...props,
-      active : true,
-      color : 'white',
-      cursor: 'pointer',
-    };
-  }
-
-  dragStart(e) {
-    this.dragged = e.currentTarget;
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.dragged);
-  }
-
-  dragEnd(e) {
-    this.dragged.style.display = 'block';
-    this.dragged.parentNode.removeChild(placeholder);
-    
-    // update state
-    var data = this.props.persons;
-    var from = Number(this.dragged.dataset.id);
-    var to = Number(this.over.dataset.id);
-    if(from < to) to--;
-    data.splice(to, 0, data.splice(from, 1)[0]);
-    this.setState({persons: data});
-  }
-
-  dragOver(e) {
-    e.preventDefault();
-    this.dragged.style.display = "none";
-    if(e.target.className === 'placeholder') return;
-    this.over = e.target;
-    e.target.parentNode.insertBefore(placeholder, e.target);
-  }
-
-	render() {
-    var listItems = this.props.persons.map((item, i) => {
-      return (
-        <li className="list-item"
-          style={{ cursor: this.state.cursor}}
-          data-id={i}
-          key={i}
-          draggable='true'
-          onDragEnd={this.dragEnd.bind(this)}
-          onDragStart={this.dragStart.bind(this)}
-          onDragOver={this.dragOver.bind(this)}>
-            Person: {item.displayFullname()} Age: {item.age} BP: {item.placeOfBirth} BD: {item.yearOfBirth}
-        </li>
-      )
-    });
-		return (
-			<ul className='task-7-list'>
-        {listItems}
-      </ul>
-		)
-	}
-}
-
-class ImageWithStatusText extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { imageStatus: "loading" };
-    }
-  
-    handleImageLoaded() {
-        alert("Loaded");
-        this.setState({ imageStatus: "loaded" });
-    }
-  
-    handleImageErrored() {
-        alert("Error");
-        this.setState({ imageStatus: "failed to load" });
-    }
-  
-    render() {
-        return (
-            <div>
-            <img className='img-test' alt = 'pokemon' 
-                src= 'https://avatanplus.com/files/resources/original/578a3ffb9cb68155f409eefa.png'
-                onLoad={this.handleImageLoaded.bind(this)}
-                onError={this.handleImageErrored.bind(this)}
-            />
-            </div>
-        );
-    }
-}
-
-
-export default class Task7 extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
+      cursor : 'pointer',
+      color : '#ff837b',
+      
       persons: [
         {
           //2-nd lvl of nesting
@@ -177,13 +89,92 @@ export default class Task7 extends Component {
           displayFullname,
         },
       ]
-    }
+    };
   }
+
+  dragStart(e) {
+    this.dragged = e.currentTarget;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.dragged);
+  }
+
+  dragEnd(e) {
+    this.dragged.style.display = 'block';
+    this.dragged.parentNode.removeChild(placeholder);
+    
+    // update state
+    var data = this.state.persons;
+    var from = Number(this.dragged.dataset.id);
+    var to = Number(this.over.dataset.id);
+    if(from < to) to--;
+    data.splice(to, 0, data.splice(from, 1)[0]);
+    this.setState({persons: data});
+  }
+
+  dragOver(e) {
+    e.preventDefault();
+    this.dragged.style.display = "none";
+    if(e.target.className === 'placeholder') return;
+    this.over = e.target;
+    e.target.parentNode.insertBefore(placeholder, e.target);
+  }
+
+	render() {
+		return (
+			<ul className='task-7-list'>
+        {this.state.persons.map((item, i) => 
+          <li className="list-item"
+            style={{ cursor: this.state.cursor, backgroundColor : this.state.color}}
+            data-id={i}
+            key={i}
+            draggable='true'
+            onDragEnd={this.dragEnd.bind(this)}
+            onDragStart={this.dragStart.bind(this)}
+            onDragOver={this.dragOver.bind(this)}>
+              Person: {item.displayFullname()} Age: {item.age} BP: {item.placeOfBirth} BD: {item.yearOfBirth}
+          </li>
+        )}
+      </ul>
+		)
+	}
+}
+
+class ImageWithStatusText extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { imageStatus: "loading" };
+    }
+  
+    handleImageLoaded() {
+        alert("Loaded");
+        this.setState({ imageStatus: "loaded" });
+    }
+  
+    handleImageErrored() {
+        alert("Error");
+        this.setState({ imageStatus: "failed to load" });
+    }
+  
+    render() {
+        return (
+            <div>
+            <img className='img-test' alt = 'pokemon' 
+                src= 'https://avatanplus.com/files/resources/original/578a3ffb9cb68155f409eefa.png'
+                onLoad={this.handleImageLoaded.bind(this)}
+                onError={this.handleImageErrored.bind(this)}
+            />
+            </div>
+        );
+    }
+}
+
+
+export default class Task7 extends Component {
 	render() {
 		return (
 		<div>
       <h2 className='task7-header'>Task7</h2>
-      <List persons={this.state.persons}/>
+        <List/>
       <ImageWithStatusText/>	
 		</div>
 		)
